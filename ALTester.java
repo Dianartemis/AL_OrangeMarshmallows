e7e3e5dfb7ad35bb68f76b04fd8797ee355eecb3
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-12-10 14:05:36 -0500

    l02

---
+++ ALTester.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ALTester {
+	ArrayList<Integer> arrList;
+	public ALTester () {
+		arrList = new ArrayList<>();
+		for(int i = 0; i < 23; i++) {
+			arrList.add(i);
+		}
+	}
+	public boolean checkIncreasing () {
+		for(int i = 0; i < 22; i++) {
+			if(arrList.get(i) > arrList.get(i+1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void main(String[]args) {
+		ALTester test = new ALTester();
+		System.out.println(test.checkIncreasing());
+		System.out.println(test.arrList);
+	}
+}
